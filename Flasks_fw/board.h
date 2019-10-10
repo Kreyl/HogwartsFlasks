0fa1ec12dcba13c69e56e1b6269bc404aaf4977f
kreyl <laystoll@yandex.ru>
2019-10-10 20:30:42 +0300

    + OS asm started

---
+++ Flasks_fw/board.h
@@ -0,0 +1,115 @@
+/*
+ * board.h
+ *
+ *  Created on: 12 сент. 2015 г.
+ *      Author: Kreyl
+ */
+
+#pragma once
+
+// ==== General ====
+#define BOARD_NAME          "HogwartsFlasks01"
+#define APP_NAME            "HogwartsFlasks"
+
+// MCU type as defined in the ST header.
+#define STM32F767xx
+
+// Freq of external crystal if any. Leave it here even if not used.
+#define CRYSTAL_FREQ_HZ 12000000
+
+// OS timer settings
+#define STM32_ST_IRQ_PRIORITY   2
+#define STM32_ST_USE_TIMER      5
+#define SYS_TIM_CLK             (Clk.GetTimInputFreq(TIM5))
+
+//  Periphery
+#define I2C1_ENABLED            FALSE
+#define I2C2_ENABLED            FALSE
+#define I2C3_ENABLED            FALSE
+#define SIMPLESENSORS_ENABLED   FALSE
+#define BUTTONS_ENABLED         FALSE
+
+#define ADC_REQUIRED            FALSE
+
+#if 1 // ========================== GPIO =======================================
+// EXTI
+#define INDIVIDUAL_EXTI_IRQ_REQUIRED    FALSE
+
+// UART
+#define UART_GPIO       GPIOA
+#define UART_TX_PIN     9
+#define UART_RX_PIN     10
+
+#endif // GPIO
+
+#if 1 // =========================== SPI =======================================
+#endif
+
+#if I2C2_ENABLED // ====================== I2C ================================
+#define I2C2_BAUDRATE   400000
+#define I2C_PILL        i2c2
+#endif
+
+#if ADC_REQUIRED // ======================= Inner ADC ==========================
+// Measuremen timing src
+#define ADC_TIM             TIM2
+
+// ADC channels
+#define ADC_VOLTAGE_CH      4
+#define ADC_CURRENT_CH      6
+#define ADC_VREFINT_CH      17 // Constant
+
+#define ADC_CHANNELS        { ADC_VOLTAGE_CH, ADC_CURRENT_CH, ADC_VREFINT_CH }
+// Index in array
+#define ADC_VOLTAGE_INDX    0
+#define ADC_CURRENT_INDX    1
+#define ADC_VREFINT_INDX    2
+
+#define ADC_CHANNEL_CNT     3   // Do not use countof(AdcChannels) as preprocessor does not know what is countof => cannot check
+#endif
+
+#if 1 // =========================== DMA =======================================
+#define STM32_DMA_REQUIRED  TRUE
+// ==== Uart ====
+// Remap is made automatically if required
+#define UART_DMA_TX     STM32_DMA_STREAM_ID(2, 7)
+#define UART_DMA_RX     STM32_DMA_STREAM_ID(2, 5)
+#define UART_DMA_CHNL   4
+#define UART_DMA_TX_MODE(Chnl) (STM32_DMA_CR_CHSEL(Chnl) | DMA_PRIORITY_LOW | STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MINC | STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_TCIE)
+#define UART_DMA_RX_MODE(Chnl) (STM32_DMA_CR_CHSEL(Chnl) | DMA_PRIORITY_MEDIUM | STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MINC | STM32_DMA_CR_DIR_P2M | STM32_DMA_CR_CIRC)
+
+#if I2C1_ENABLED // ==== I2C1 ====
+#define I2C1_DMA_TX     STM32_DMA1_STREAM6
+#define I2C1_DMA_RX     STM32_DMA1_STREAM5
+#endif
+#if I2C2_ENABLED // ==== I2C2 ====
+#define I2C2_DMA_TX     STM32_DMA1_STREAM7
+#define I2C2_DMA_RX     STM32_DMA1_STREAM3
+#endif
+
+#if ADC_REQUIRED
+#define ADC_DMA         STM32_DMA_STREAM_ID(2, 0)
+#define ADC_DMA_MODE    STM32_DMA_CR_CHSEL(0) |   /* DMA2 Stream0 Channel0 */ \
+                        DMA_PRIORITY_LOW | \
+                        STM32_DMA_CR_MSIZE_HWORD | \
+                        STM32_DMA_CR_PSIZE_HWORD | \
+                        STM32_DMA_CR_MINC |       /* Memory pointer increase */ \
+                        STM32_DMA_CR_DIR_P2M |    /* Direction is peripheral to memory */ \
+                        STM32_DMA_CR_CIRC |       /* Circulate */ \
+                        STM32_DMA_CR_TCIE         /* Enable Transmission Complete IRQ */
+#endif // ADC
+
+#endif // DMA
+
+#if 1 // ========================== USART ======================================
+#define PRINTF_FLOAT_EN FALSE
+#define UART_TXBUF_SZ   8192
+#define UART_RXBUF_SZ   99
+
+#define UARTS_CNT       1
+
+#define CMD_UART_PARAMS \
+    USART1, UART_GPIO, UART_TX_PIN, UART_GPIO, UART_RX_PIN, \
+    UART_DMA_TX, UART_DMA_RX, UART_DMA_TX_MODE(UART_DMA_CHNL), UART_DMA_RX_MODE(UART_DMA_CHNL)
+
+#endif
---
+++ Flasks_fw/main.cpp
@@ -0,0 +1,76 @@
+#include "hal.h"
+#include "MsgQ.h"
+#include "kl_lib.h"
+#include "Sequences.h"
+#include "uart.h"
+#include "shell.h"
+//#include "led.h"
+//#include "buttons.h"
+//#include "PinSnsSettings.h"
+
+#if 1 // =============== Defines ================
+// Forever
+EvtMsgQ_t<EvtMsg_t, MAIN_EVT_Q_LEN> EvtQMain;
+static const UartParams_t CmdUartParams(115200, CMD_UART_PARAMS);
+CmdUart_t Uart{&CmdUartParams};
+static void ITask();
+static void OnCmd(Shell_t *PShell);
+
+static TmrKL_t TmrOneSecond {TIME_MS2I(999), evtIdEverySecond, tktPeriodic};
+#endif
+
+int main() {
+    // ==== Setup clock ====
+//    Clk.SetCoreClk(cclk48MHz);
+//    Clk.UpdateFreqValues();
+
+    // ==== Init OS ====
+    halInit();
+    chSysInit();
+
+    // ==== Init Hard & Soft ====
+    EvtQMain.Init();
+    Uart.Init();
+    Printf("\r%S %S\r\n", APP_NAME, XSTRINGIFY(BUILD_TIME));
+//    Clk.PrintFreqs();
+
+//    TmrOneSecond.StartOrRestart();
+
+    // ==== Main cycle ====
+    ITask();
+}
+
+__noreturn
+void ITask() {
+    while(true) {
+        EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
+        switch(Msg.ID) {
+            case evtIdShellCmd:
+                OnCmd((Shell_t*)Msg.Ptr);
+                ((Shell_t*)Msg.Ptr)->SignalCmdProcessed();
+                break;
+
+            case evtIdEverySecond:
+//                Printf("Second\r");
+                break;
+
+            default: break;
+        } // switch
+    } // while true
+}
+
+#if 1 // ======================= Command processing ============================
+void OnCmd(Shell_t *PShell) {
+    Cmd_t *PCmd = &PShell->Cmd;
+//    Printf("%S  ", PCmd->Name);
+
+    // Handle command
+    if(PCmd->NameIs("Ping")) PShell->Ack(retvOk);
+    else if(PCmd->NameIs("Version")) PShell->Print("%S %S\r\n", APP_NAME, XSTRINGIFY(BUILD_TIME));
+
+    else {
+        Printf("%S\r\n", PCmd->Name);
+        PShell->Ack(retvCmdUnknown);
+    }
+}
+#endif
